138e9839ee7e506cb45957a4a41991e2447b2b7e
190267 <190267@upf.br>
2026-04-15 21:49:19 -0300

    Update index.js

--- services/controller/index.js
+++ services/controller/index.js
@@ -6,9 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-/**
- * Retorna a lista de produtos da loja via InventoryService
- */
+
 app.get('/products', (req, res, next) => {
     inventory.SearchAllProducts(null, (err, data) => {
         if (err) {
@@ -20,9 +18,7 @@ app.get('/products', (req, res, next) => {
     });
 });
 
-/**
- * Consulta o frete de envio no ShippingService
- */
+
 app.get('/shipping/:cep', (req, res, next) => {
     shipping.GetShippingRate(
         {
@@ -42,27 +38,22 @@ app.get('/shipping/:cep', (req, res, next) => {
     );
 });
 
-// Passo 4: Nova rota para buscar produto por ID
+
 app.get('/product/:id', (req, res, next) => {
-    // Chama método do microsserviço.
+
     inventory.SearchProductByID({ id: req.params.id }, (err, product) => {
-        // Se ocorrer algum erro de comunicação
-        // com o microsserviço, retorna para o navegador.
+
         if (err) {
             console.error(err);
             res.status(500).send({ error: 'something failed :(' });
         } else {
-            // Caso contrário, retorna resultado do
-            // microsserviço (um arquivo JSON) com os dados
-            // do produto pesquisado
+
             res.json(product);
         }
     });
 });
 
-/**
- * Inicia o router
- */
+
 app.listen(3000, () => {
     console.log('Controller Service running on http://127.0.0.1:3000');
 });
